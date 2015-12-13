02d6980efa293a5fd9878735ab7fdad58eabcf33
Trung Dinh <trung.dinh@tubemogul.com>
2015-12-13 10:03:22 -0800

    update heap

--- java_algorithms/sort/HeapSort.java
+++ java_algorithms/sort/HeapSort.java
@@ -1,7 +1,8 @@
 public class HeapSort {
 	public static void main(String[] ags){
-		int[] test = new int[]{1,2,3,4,5};
-		for(int i = test.length/2 ; i >0; i--){
+		int[] test = new int[]{16,4,10,14,7,9,3,2,8,1};
+		int middleIndex = test.length/2 - 1;
+		for(int i = middleIndex ; i > -1; i--){
 			maxHeapify(test, i);
 		}
 		StringBuilder strBuilder = new StringBuilder();
@@ -13,22 +14,22 @@ public class HeapSort {
 	}
 	
 	public static void maxHeapify(int[] array, int i){
-		int left = 2*i;
-		int right = 2*i + 1;
-		log("left: " + left + " right: " + right);
-		if( left >= array.length || right >= array.length){
+		int left = 2*i+1;
+		int right = 2*i+2;
+		if( left > array.length -1 || right > array.length -1){
+			log("Left or right index is greater than: " + (array.length -1));
 			return;
 		}
-	
 		int maxChild = Math.max( array[left], array[right]);
-		
 		if( array[ i ] < maxChild){
 			//if left
 			if( maxChild == array[left]){
+				log("Left child index: " + left + " value: " + array[left]);
 				swap(array, i, left);
 				maxHeapify(array, left);
 			}
 			else{
+				log("Right child index: " + right + " value: " + array[right]);
 				swap(array,i, right);
 				maxHeapify(array, right);
 			}
@@ -36,7 +37,7 @@ public class HeapSort {
 		}
 	}	
 	static void log(Object msg){
-	System.out.println(msg.toString());
+		System.out.println(msg.toString());
 	}
 	public static void swap(int[] array, int index1, int index2){
 			int temp = array[index1];
